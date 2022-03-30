a51e6409fe50fda91bab3a8bf7f1ffbeccb960ef
danielgil <danistcruz@gmail.com>
2022-03-29 20:59:45 -0500

    proyecto inicial para empezar a realizar el sistema de productos

---
+++ products/src/main/java/com/sofka/products/SistemaDemoDeProductsApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDemoDeProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDemoDeProductsApplication.class, args);
+	}
+
+}

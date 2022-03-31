3cb99bb79e3aafc12bae403aae95b336f7be90d6
danielgil <danistcruz@gmail.com>
2022-03-31 00:05:41 -0500

    se modifico ya que no se habian añadido las dependencias y de creo la conexion con la base de datos

--- products/src/main/java/com/sofka/products/ProductsApplication.java
+++ products/src/main/java/com/sofka/products/ProductsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaDemoDeProductsApplication {
+public class ProductsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaDemoDeProductsApplication.class, args);
+		SpringApplication.run(ProductsApplication.class, args);
 	}
 
 }
--- products/src/test/java/com/sofka/products/ProductsApplicationTests.java
+++ products/src/test/java/com/sofka/products/ProductsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SistemaDemoDeProductsApplicationTests {
+class ProductsApplicationTests {
 
 	@Test
 	void contextLoads() {
